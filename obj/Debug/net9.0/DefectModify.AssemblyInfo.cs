1b7dad87a09c72b11670b626af0eebd2f7131e87
minhchien137 <bmc13072001@gmail.com>
2026-07-24 15:05:22 +0700

    full

--- obj/Debug/net9.0/DefectModify.AssemblyInfo.cs
+++ obj/Debug/net9.0/DefectModify.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DefectModify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fffa3e594f8235992141a67ed13c1cbc9ad887de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c15af0412982be52b55ed5223ff397ddc06b6d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DefectModify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DefectModify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
